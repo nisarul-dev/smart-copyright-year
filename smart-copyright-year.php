67bd62b5470275a0b4aa537dad19cc9c9b855dfb
Nisarul Amin Naim <65971192+nisarul-media@users.noreply.github.com>
2023-08-08 03:39:46 +0600

    Plugin Bootstrapping

---
+++ smart-copyright-year.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Smart Copyright Year
+ * Plugin URI: https://github.com/nisarul-media/smart-copyright-year
+ * Description: Auto-update copyright year with [year] shortcode. No manual updates needed! Maintain a pro look effortlessly.
+ * Version: 1.0.0
+ * Author: Nisarul Amin Naim
+ * Author URI: https://www.nisarul.com
+ * License: GPLv3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: smart-copyright-year
+ */
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with This program. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+*/
+
+defined( 'ABSPATH' ) || exit;
