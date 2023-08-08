960d841cdb375df033dc97152aff75e1d17de128
Nisarul Amin Naim <65971192+nisarul-media@users.noreply.github.com>
2023-08-09 04:45:25 +0600

    Update smart-copyright-year.php
    Created Shortcode

--- smart-copyright-year.php
+++ smart-copyright-year.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Smart Copyright Year
- * Plugin URI: https://github.com/nisarul-media/smart-copyright-year
+ * Plugin URI: https://github.com/nisarul-dev/smart-copyright-year
  * Description: Auto-update copyright year with [year] shortcode. No manual updates needed! Maintain a pro look effortlessly.
  * Version: 1.0.0
  * Author: Nisarul Amin Naim
@@ -26,4 +26,29 @@ You should have received a copy of the GNU General Public License
 along with This program. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
 */
 
-defined( 'ABSPATH' ) || exit;
+defined( 'ABSPATH' ) || exit; // Prevent direct access
+
+/**
+ * The [year] shortcode.
+ *
+ * Displays the current year.
+ *
+ * @param array  $atts    Shortcode attributes. Default empty.
+ * @param string $content Shortcode content. Default null.
+ * @param string $tag     Shortcode tag (name). Default empty.
+ * @return string Shortcode output.
+ */
+function scy_year_shortcode( $atts = [], $content = null, $tag = '' ) {
+	$output = esc_html( date( 'Y' ) );
+
+	return $output;
+}
+
+/**
+ * Central location to create all shortcodes.
+ */
+function scy_shortcodes_init() {
+	add_shortcode( 'year', 'scy_year_shortcode' );
+}
+
+add_action( 'init', 'scy_shortcodes_init' );
